c90b4e0921a63a90101812570df8bf48be10ddb6
Marika Ross <marika.ross@gmail.com>
2018-09-04 10:49:26 -0600

    Begin file set up for server tests

--- server.js
+++ server.js
@@ -21,6 +21,34 @@ app.get('/api/v1/list_items', (request, response) => {
   })
 })
 
+app.post('/api/v1/list_items', (request, response) => {
+  const idea = request.body;
+
+  for (let requiredParameter of ['title', 'description']) {
+    if(!idea[requiredParameter]) {
+      return response.status(422).send({error: `Expected format: {title: <STRING>, description: <STRING>}. You are missing a "${requiredParameter} property"`})
+    }
+  }
+  database('list_items').insert(idea, 'id')
+    .then(list_item => {
+      response.status(201).json({id: list_item[0]})
+    })
+    .catch(error => {
+      response.status(500).json( {error} )
+    });
+});
+
+app.delete('/api/v1/list_items/:id', (request, response) => {
+  database('list_items').where('id', request.params.id).del()
+  .then(list_item => {
+    if (list_item.length) {
+      response.status(404).json({error: `Could not find a list item with id ${request.params.id}`})
+    } else {
+      response.status(200).json(list_item)
+    }
+  })
+})
+
 
 
 app.listen(app.get('port'), () => {
---
+++ test/routes.spec.js
@@ -0,0 +1,51 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const knex = require('../db/knex');
+
+chai.use(chaiHttp);
+
+describe('api endpoints', () => {
+  beforeEach( done => {
+    knex.migrate.rollback()
+    .then(() => {
+      knex.migrate.latest()
+      .then(() => {
+        return knex.seed.run()
+        .then(() =>{
+          done()
+        })
+      })
+    })
+  })
+  describe('GET to /api/v1/list_items', () => {
+    it('should return all list_items', done => {
+      chai.request(server)
+      .get('/api.v1/list_items')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(2);
+        response.body[0].should.have.property('title');
+        response.body[0].title.should.equal('Australia');
+        response.body[0].should.have.property('description');
+        response.body[0].description.should.equal('Go see a platypus in person');
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        done()
+      })
+    })
+    it('should return a 404 for a route that does not exist', done => {
+      chai.request(server)
+      .get('/sad')
+      .end((err, response) => {
+        response.should.have.status(404);
+      done()
+      })
+  })
+  })
+})
