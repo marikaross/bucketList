799ccd4728fb0affb41dc26f15d9269e53efffb1
Marika Ross <marika.ross@gmail.com>
2018-09-04 10:22:49 -0600

    Make get endpoint for list items

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('src'));
+
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Bucket List';
+
+app.get('/api/v1/list_items', (request, response) => {
+  database('list_items').select()
+  .then((list_items) => {
+    response.status(200).json(list_items)
+  }).catch((error) => {
+    response.status(500).json( {error} )
+  })
+})
+
+
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
